8a2f650511721af42aa1603d1d1391680e056b22
Adam Labiosa <atlabiosa@gmail.com>
2022-02-24 12:26:25 -0600

    Project and contributor additions

--- HashCode2022/src/Contributer.java
+++ HashCode2022/src/Contributer.java
@@ -1,8 +1,8 @@
 import java.util.HashMap;
 
 public class Contributer {
-    String name;
-    HashMap<String, Integer> skillMap = new HashMap<String, Integer>();
+    private String name;
+    private HashMap<String, Integer> skillMap = new HashMap<String, Integer>();
 
     public Contributer(String name, String[] skills, int[] levels){
         this.name = name;
@@ -10,6 +10,18 @@ public class Contributer {
         for(int i = 0; i < skills.length; i++){
             skillMap.put(skills[i], levels[i]);
         }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getLevel(String skill){
+        return skillMap.get(skill);
+    }
+
+    public void changeLevel(String skill, int newLevel){
+        skillMap.put(skill, newLevel);
     }
 
 }
--- HashCode2022/src/Project.java
+++ HashCode2022/src/Project.java
@@ -1,10 +1,10 @@
 
 public class Project {
-	private String name;
-	private int dur;
-	private int score;
-	private int bestBefore;
-	private String[] roles;
+	private String name; //Name of project
+	private int dur; //Duration of project
+	private int score; //Score project is worth
+	private int bestBefore; //Number of days before project decays
+	private String[] roles; //Roles that project needs
 
 	public Project(String name, int dur, int score, int bestBefore, String[] roles){
 		this.name = name;
@@ -14,4 +14,23 @@ public class Project {
 		this.roles = roles;
 	}
 
+	public String getName(){
+		return this.name;
+	}
+
+	public int getDur(){
+		return this.dur;
+	}
+
+	public int getScore(){
+		return this.score;
+	}
+
+	public int getBestBefore(){
+		return this.bestBefore;
+	}
+
+	public String[] getRoles(){
+		return this.roles;
+	}
 }
